60fbc60bf4ed5942c6405a12e684dc5318716255
Ankit77760 <ankitm7972@gmail.com>
2024-04-12 15:16:54 +0530

    Return Vector of Vector of Paths With Given Sum

--- Paths_With_Given_Sum.cpp
+++ Paths_With_Given_Sum.cpp
@@ -1,3 +1,28 @@
 vector<vector<int> > paths(TreeNode *root, int sum)
 {
+    vector<vector<int> > ans;
+    vector<int> temp;
+    helper(root, sum, ans, temp);
+    return ans;
+}
+
+void helper(TreeNode *node, int sum, vector<vector<int> > &ans, vector<int> &temp)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+
+    temp.push_back(node->data);
+
+    if (node->data == sum && node->left == nullptr && node->right == nullptr)
+    {
+        ans.push_back(temp);
+    }
+    else
+    {
+        helper(node->left, sum - node->data, ans, temp);
+        helper(node->right, sum - node->data, ans, temp);
+    }
+    temp.pop_back();
 }
